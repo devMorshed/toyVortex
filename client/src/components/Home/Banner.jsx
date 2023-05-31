203c4d29cdc5a8310488c665ef4baaa3847eb0d5
devMorshed <ac3m0r@gmail.com>
2023-05-31 17:00:00 +0600

    banner img optimized

--- client/src/components/Home/Banner.jsx
+++ client/src/components/Home/Banner.jsx
@@ -1,10 +1,12 @@
-import ace from "../../assets/ace.gif";
+
 const Banner = () => {
 	return (
 		<div className="bg-indigo-400 py-20 rounded-lg ">
 			<div className="max-w-7xl mx-auto px-4">
 				<div className="flex items-center justify-between flex-col  md:flex-row gap-10">
-					<div data-aos="fade-right" className="md:w-1/2 text-center ">
+					<div
+						data-aos="fade-right"
+						className="md:w-1/2 text-center ">
 						<h1 className="text-4xl text-white font-bold mb-6">
 							Welcome to Toy Vortex!
 						</h1>
@@ -16,7 +18,11 @@ const Banner = () => {
 						</button>
 					</div>
 					<div className="md:w-1/2">
-						<img src={ace} alt="Toy World" className="w-full " />
+						<img
+							src="https://i.ibb.co/BjcLT6J/luffy-smile.gif"
+							alt="Toy World"
+							className="w-full rounded-xl "
+						/>
 					</div>
 				</div>
 			</div>
